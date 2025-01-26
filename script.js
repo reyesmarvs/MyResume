e5ccedf12b9a384b6530ba6321544552fbc28232
reyesmarvs <marvinclr@gmail.com>
2025-01-27 01:39:05 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/*Dark Mode*/
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+/*END*/
+
+
+/*Nav Open & Close Button*/
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+})
+
+// Close menu when close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+/*END*/
