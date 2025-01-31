1007b1a8e4d9aaa5acb9cba219d4e8e4d3b69e3e
reyesmarvs <marvinclr@gmail.com>
2025-02-01 00:25:58 +0800

    v3 update

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*Fade In Upon Scroll*/
+let elementsArray = document.querySelectorAll(".container");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn();
+/*END*/
+
+
+
 /*Dark Mode*/
 let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
